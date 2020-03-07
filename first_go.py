90ca6baba5211a44e6ea1c97a01389a5a6f0c3b2
Mariia <mariia.artemova@gmail.com>
2020-03-07 16:29:45 +0100

    version 3

--- first_go.py
+++ first_go.py
@@ -39,35 +39,38 @@ def kitchen_sink(vY, mX, vX_new):
     return yFit
 
 
+def WeightedForecast(y, X, x_pred):
+    predictions = np.zeros(np.shape(X)[1])
+    for i in range(np.shape(X)[1]-1):
+        xx = X[:, [0, i+1]]
+        xx_pred = x_pred[[0, i+1]]
+        model = stats.regression.linear_model.OLS(y, xx)
+        results = model.fit()
+        predictions[i] = results.predict(xx_pred)
+    y_hat = np.mean(predictions)
+    return y_hat
+
+
+def AR1(y, X, x_pred):
+    xx = X[:, 0:2]
+    xx_pred = x_pred[0:2]
+    model = stats.regression.linear_model.OLS(y, xx)
+    results = model.fit()
+    y_hat = results.predict(xx_pred)
+    return y_hat
+
 """ Big loop to get predictions """
 
 windows = window_fct()
 y_KS = []
+y_WF = []
+y_AR = []
 for window in windows:
     X = mX[window[0]:window[1], :]
     X_new = mX[window[1], :]
     Y = vY[window[0]:window[1]]
     y_KS += [kitchen_sink(Y, X, X_new)]
+    y_WF += [WeightedForecast(Y, X, X_new)]
+    y_AR += [AR1(Y, X, X_new)]
 
-def WeightedForecast(y, X, x_pred, y_pred):
-    predictions = np.zeros(np.shape(X)[1])
-    for i in range(np.shape(X)[1]):
-        xx = X[:, i]
-        xx = stats.add_constant(xx)
-        xx_pred = np.hstack((1, x_pred.iloc[i]))
-        model = stats.regression.linear_model.OLS(y, xx)
-        results = model.fit()
-        predictions[i] = results.predict(xx_pred)
-    y_hat = np.mean(predictions)
-    error = y_pred - y_hat
-    return error
 
-def AR1(y, X, x_pred, y_pred):
-    xx = X[:, 0]
-    xx = stats.add_constant(xx)
-    xx_pred = np.hstack((1, x_pred[0]))
-    model = stats.regression.linear_model.OLS(y, xx)
-    results = model.fit()
-    y_hat = results.predict(xx_pred)
-    error = y_pred - y_hat
-    return error
