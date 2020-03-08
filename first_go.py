12b4ac3364a7aa2dee905e92250c59eed6297464
Igor Custodio <custodioij@gmail.com>
2020-03-08 16:04:22 +0100

    Added CSR without lags

--- first_go.py
+++ first_go.py
@@ -4,7 +4,7 @@ import numpy as np
 import OLS_func as ols
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+import itertools as iter
 
 
 data = pd.read_excel("USEMP.xlsx")
@@ -41,7 +41,7 @@ def kitchen_sink(vY, mX, vX_new, y_new):
     """
     betas = ols.EstimateMM(vY, mX)
     yFit = ols.OLS_predict(vX_new, betas)
-    return [yFit - y_new, yFit]
+    return yFit - y_new
 
 
 def WeightedForecast(y, X, x_pred, y_pred):
@@ -54,7 +54,7 @@ def WeightedForecast(y, X, x_pred, y_pred):
         predictions[i] = results.predict(xx_pred)
     y_hat = np.mean(predictions)
     error = y_pred - y_hat
-    return [error, y_hat]
+    return error
 
 
 def AR1(y, X, x_pred, y_pred):
@@ -64,7 +64,7 @@ def AR1(y, X, x_pred, y_pred):
     results = model.fit()
     y_hat = results.predict(xx_pred)
     error = y_pred - y_hat
-    return [error[0], y_hat[0]]
+    return error
 
 
 def factor_augmented(vY, mX, vX_new, y_new):
@@ -84,41 +84,64 @@ def factor_augmented(vY, mX, vX_new, y_new):
     betas = ols.EstimateMM(vY, PC)
     yFit = ols.OLS_predict(PC_new, betas)
     error = yFit - y_new
-    return [error, yFit]
+    return error
+
+
+def csr(vY, mX, vX_new, y_new, lag=False, k=None):
+    mX = mX[:, 1:]  # Remove constant
+    vX_new = vX_new[1:]
+    if lag:
+        current = mX[1:, :]
+        lagged = mX[:-1, :]
+        mX = np.hstack((current, lagged))
+    if k is None:
+        k = mX.shape[1] # Account for the constant!
+    # Create combinations
+    l_subset = []
+    for i in range(k):
+        l_subset += list(iter.combinations([j for j in range(k)], i+1))
+    l_yfit = []
+    for t_subset in l_subset:
+        xx = stats.add_constant(mX[:, t_subset])
+        xnew = np.hstack((1, vX_new[list(t_subset)]))
+        betas = ols.EstimateMM(vY, xx)
+        l_yfit += [ols.OLS_predict(xnew, betas)]
+    error = y_new - np.mean(l_yfit)
+    return error
 
 
 """ Big loop to get predictions """
 
 windows = window_fct()
-e_KS, yFit_KS = [], []
-e_WF, yFit_WF = [], []
-e_AR, yFit_AR = [], []
-e_FA, yFit_FA = [], []
+e_KS = []
+e_WF = []
+e_AR = []
+e_FA = []
 e_mean = []
+e_csr = []
 for window in windows:
     X = mX[window[0]:window[1], :]
     X_new = mX[window[1], :]
     Y = vY[window[0]:window[1]]
     y_new = vY[window[1]]
-    e_KS += [kitchen_sink(Y, X, X_new, y_new)[0]]
-    e_WF += [WeightedForecast(Y, X, X_new, y_new)[0]]
-    e_AR += [AR1(Y, X, X_new, y_new)[0]]
-    e_FA += [factor_augmented(Y, X, X_new, y_new)[0]]
-    yFit_KS += [kitchen_sink(Y, X, X_new, y_new)[1]]
-    yFit_WF += [WeightedForecast(Y, X, X_new, y_new)[1]]
-    yFit_AR += [AR1(Y, X, X_new, y_new)[1]]
-    yFit_FA += [factor_augmented(Y, X, X_new, y_new)[1]]
+    e_KS += [kitchen_sink(Y, X, X_new, y_new)]
+    e_WF += [WeightedForecast(Y, X, X_new, y_new)]
+    e_AR += [AR1(Y, X, X_new, y_new)]
+    e_FA += [factor_augmented(Y, X, X_new, y_new)]
     e_mean += [np.mean(Y) - y_new]
+    e_csr += [csr(Y, X, X_new, y_new)]
 
 """ Comparison """
 # RMSE:
 rmse = lambda xx: np.sqrt(np.mean([x**2 for x in xx]))
 mafe = lambda xx: np.mean(np.abs([x for x in xx]))
 
-df_rmse = np.vstack([rmse(e_mean), rmse(e_AR), rmse(e_KS), rmse(e_WF), rmse(e_FA)]).T
-df_mafe = np.vstack([mafe(e_mean), mafe(e_AR), mafe(e_KS), mafe(e_WF), mafe(e_FA)]).T
+
+df_rmse = np.vstack([rmse(e_mean), rmse(e_AR), rmse(e_KS), rmse(e_WF), rmse(e_FA), rmse(e_csr)]).T
+df_mafe = np.vstack([mafe(e_mean), mafe(e_AR), mafe(e_KS), mafe(e_WF), mafe(e_FA), mafe(e_csr)]).T
 mOutM= np.vstack([df_rmse, df_mafe])
-dfOut1 = pd.DataFrame(mOutM, columns =['$Mean model$', '$AR(1)$', '$Kitchen-sink$', '$Weighted forecast$', '$FAVAR$'], index=['MSFE','MAFE'])
+dfOut1 = pd.DataFrame(mOutM, columns=['$Mean model$', '$AR(1)$', '$Kitchen-sink$', '$Weighted forecast$', '$FAVAR$', '$CSR$'],
+                      index=['MSFE','MAFE'])
 print(dfOut1.to_latex(escape=False))
 print(rmse(e_mean))
 print(rmse(e_AR))
@@ -126,37 +149,12 @@ print(rmse(e_AR))
 print(rmse(e_KS))
 print(rmse(e_WF))
 print(rmse(e_FA))
-
-
-def DieboldMarianoTest(mE, Name, loss):
-    '''
-    Diebold-Mariano test
-    '''
-    iT = np.shape(mE)[1]
-    mRes = np.zeros((iT, iT))
-    for i in range(iT):
-        for j in range(i + 1):
-            if loss == 'RMSE':
-                d = mE[:, i] ** 2 - mE[:, j] ** 2
-            if loss == 'MAFE':
-                d = np.abs(mE[:, i]) - np.abs(mE[:, j])
-            dmean = np.mean(d)
-            dstd = np.std(d)
-            test = np.sqrt(np.shape(d)[0]) * dmean / dstd
-            mRes[j, i] = (1 - norm.cdf(np.abs(test))) * 2
-    dfDM = pd.DataFrame(np.round(mRes, 4), index=Name, columns=Name)
-    print(dfDM.to_latex(escape=False))
-
-mE = np.vstack(np.array([e_mean, e_AR, e_KS, e_WF, e_FA])).T
-Name = np.array(('$Mean model$', '$AR(1)$', '$Kitchen-sink$', '$Weighted forecast$', '$FAVAR$'))
-DieboldMarianoTest(mE, Name, 'RMSE')
-DieboldMarianoTest(mE, Name, 'MAFE')
-
+print(rmse(e_csr))
 
 """ Investigate instability """
 # Calculate 2-year moving average of forecast errors and plot
 
-errors = [e_AR, e_mean, e_KS, e_WF, e_FA]
+errors = [e_AR, e_mean, e_KS, e_WF, e_FA, e_csr]
 moving_avg = []
 
 for error in errors:
@@ -167,35 +165,9 @@ for error in errors:
 
 to_plot = pd.DataFrame(dt[-len(moving_avg[1]):])
 to_plot = pd.concat([to_plot, pd.DataFrame(np.array(moving_avg).T, index=to_plot.index)], axis=1)
-to_plot.columns = ['Month', 'AR', 'Mean', 'KS', 'WF', 'FA']
+to_plot.columns = ['Month', 'AR', 'Mean', 'KS', 'WF', 'FA', 'CSR']
 
 # plt.close('all')
 # fig = plt.figure()
 to_plot.plot(x='Month')
 plt.show()
-
-
-moving_avg_abs = []
-for error in errors:
-    local_mean_abs = []
-    for t in range(len(error) - 24):
-     local_mean_abs += [mafe(error[t:(t+24)])]
-    moving_avg_abs += [local_mean_abs]
-
-to_plot2 = pd.DataFrame(dt[-len(moving_avg_abs[1]):])
-to_plot2 = pd.concat([to_plot2, pd.DataFrame(np.array(moving_avg_abs).T, index=to_plot2.index)], axis=1)
-to_plot2.columns = ['Month', 'AR', 'Mean', 'KS', 'WF', 'FA']
-to_plot2.plot(x='Month')
-plt.show()
-
-Columns = np.array(('AR(1)', 'Kitchen-sink', 'FAVAR'))
-yFit = np.array((yFit_AR, yFit_KS, yFit_FA)).T
-ii = dt[(dt == "1980-01-01")].index[0]
-for i in range(3):
-    to_plot3 = pd.DataFrame(dt[ii:])
-    to_plot3 = pd.concat([to_plot3, pd.DataFrame(np.vstack((data.iloc[ii:, 1], yFit[:,i])).T, index=to_plot3.index)], axis=1)
-    to_plot3 = to_plot3.set_index('Month')
-    to_plot3.columns = ['true', Columns[i]]
-    to_plot3.plot()
-    plt.savefig(Columns[i] + ' forecast.png')
-    plt.show()
